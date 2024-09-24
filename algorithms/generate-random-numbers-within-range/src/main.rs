954d27e4dbc0cbede2ead60680f06bc4a6e621aa
Dehvon Curtis <dcurtis@datto.com>
2024-09-24 14:46:38 -0600

    added `Uniform`;

--- algorithms/generate-random-numbers-within-range/src/main.rs
+++ algorithms/generate-random-numbers-within-range/src/main.rs
@@ -1,7 +1,25 @@
-use rand::Rng;
 
+// Without Uniform
+// use rand::Rng;
+
+// fn main() {
+//     let mut rng = rand::thread_rng();
+//     println!("Integer: {}", rng.gen_range(0..10));
+//     println!("Float: {}", rng.gen_range(0.0..10.0));
+// }
+
+
+// With Uniform
+use rand::distributions::{Distribution, Uniform};
 fn main() {
     let mut rng = rand::thread_rng();
-    println!("Integer: {}", rng.gen_range(0..10));
-    println!("Float: {}", rng.gen_range(0.0..10.0));
+    let die = Uniform::from(1..7);
+
+    loop {
+        let throw = die.sample(&mut rng);
+        println!("Roll the die: {}", throw);
+        if throw == 6 {
+            break;
+        }
+    }
 }
